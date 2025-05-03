ac4f52be8833b8e1c2f8b6266b5021204aeb3ded
lamiz-khan <khanlamiz8@gmail.com>
2025-05-03 20:37:20 +0500

    maximum value of list

--- main.dart
+++ main.dart
@@ -1,8 +1,8 @@
-void main(){
-List numbers = [55,66,3,44,890,321,1,5000,43,9];
-print("list = $numbers");
-numbers.sort();
-print("sorting list = $numbers");
-print(numbers.first);
-print(numbers.last);
+void main() {
+  List numbers = [55, 66, 3, 44, 890, 321, 1, 5000, 43, 9];
+  print("list = $numbers");
+  numbers.sort();
+  var Rev = numbers.reversed;
+  print("reverse list = $Rev");
+  print(Rev.first);
 }
